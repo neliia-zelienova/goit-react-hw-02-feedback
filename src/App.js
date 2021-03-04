67e4fb50d95567771386148e60a550e455615c6c
neliia-zelienova <66479463+neliia-zelienova@users.noreply.github.com>
2021-03-04 21:53:46 +0200

    Replace logic from LeaveFeedback to App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,58 @@
 import "./App.css";
-import LeaveFeedback from "./components/LeaveFeedback";
+import React from "react";
+import Statistics from "./components/Statistics/Statistics";
+import FeedbackOptions from "./components/FeedbackOptions/FeedbackOptions";
+import Section from "./components/Section/Section";
 import Container from "./components/Container";
 
-function App() {
-  return (
-    <Container>
-      <LeaveFeedback />
-    </Container>
-  );
+class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.bad + this.state.neutral;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    if (this.countTotalFeedback())
+      return Math.round((this.state.good / this.countTotalFeedback()) * 100.0);
+    else return 0;
+  };
+
+  handleClick = (e) => {
+    const option = e.currentTarget.name;
+    this.setState((prevState) => ({
+      [option]: prevState[option] + 1,
+    }));
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <Container>
+        <div>
+          <Section title="Please leave feedback">
+            <FeedbackOptions
+              options={["good", "neutral", "bad"]}
+              onLeaveFeedback={this.handleClick}
+            />
+          </Section>
+          <Section title="Statistics">
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          </Section>
+        </div>
+      </Container>
+    );
+  }
 }
 
 export default App;
